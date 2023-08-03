62341db440e7197668f6d12a7137d19ac0f4c037
didillysquat <didillysquat@gmail.com>
2023-08-03 10:03:14 +0100

    remove mt prefix function from features_names

--- bin/features_names.py
+++ bin/features_names.py
@@ -2,7 +2,10 @@
 """
 Script to produce the _features_names.tsv used in the count_matrix process.
 
-Takes in a .gtf file, filters down to XXX and XXX.
+Takes in a .gtf file, and creates a tsv with two columns:
+    ensID and geneSym
+
+These are the gene ID and gene Name
 """
 
 import math
@@ -31,17 +34,6 @@ def replace_na_gene_names_with_gene_id(gene_id, gene_name):
         gene_name2 = gene_name
     return(gene_name2)
 
-def annotate_mito_genes_with_mt_prefix(chromosome, gene_name):
-    """
-    Annotate the MT Genes with a "MT_" prefix
-    """
-    if chromosome == "MT" and gene_name != "InterGenic":
-        gene_name2 = chromosome + "_" + gene_name
-    else:
-        # do not annotate if not mitochondrial
-        gene_name2 = gene_name
-    return gene_name2
-
 # Get in and out paths from 
 gtf_in_path = sys.argv[1]
 feature_names_out_path = sys.argv[2]
@@ -54,8 +46,7 @@ feature_names_obj = gtf_obj.drop_duplicates()
 # Split chromosome list and keep first value (they are all identical)
 feature_names_obj['seqname'] = feature_names_obj.seqname.str.split(";", expand=True)[0]
 
-feature_names_obj['gene_name1'] = feature_names_obj.apply(lambda x: replace_na_gene_names_with_gene_id(x['gene_id'], x['gene_name']), axis=1)
-feature_names_obj['gene_name'] = feature_names_obj.apply(lambda x: annotate_mito_genes_with_mt_prefix(x['seqname'], x['gene_name1']), axis=1)
+feature_names_obj['gene_name'] = feature_names_obj.apply(lambda x: replace_na_gene_names_with_gene_id(x['gene_id'], x['gene_name']), axis=1)
 
 # Select and rename columns
 feature_names_obj = feature_names_obj.loc[:,['gene_id','gene_name']]
